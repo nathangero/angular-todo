2b32159e3b841d0bc39fff59962df29c4ff4393c
Nathan Geronimo <nathangero@yahoo.com>
2024-10-22 14:34:55 -0700

    create todo-list component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TodoFormComponent } from "../todo-form/todo-form.component";
+import { TodoListComponent } from "../todo-list/todo-list.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TodoFormComponent],
+  imports: [TodoFormComponent, TodoListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
